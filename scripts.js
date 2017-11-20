4485d6e2407006c32a9052f20b2c3fa90179795b
Bzakrzewski <30876128+Bzakrzewski@users.noreply.github.com>
2017-11-20 17:24:54 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,19 +1,18 @@
-var bckColor = [
-     "#D79938", '#C73A3C', '#0A96D6', '#B6D749', '#D7CD16'
-];
-
-document.querySelector('.btn-change').addEventListener('click', changeColor);
+var bckColor = ["#D79938", '#C73A3C', '#0A96D6', '#B6D749', '#D7CD16'];
 var nmb;
 
-function changeColor(){ 
+document.querySelector('.btn-change').addEventListener('click', function (){ 
    var dbl = nmb;
     nmb = Math.floor(Math.random() * 5);
     while(nmb==dbl){
         nmb = Math.floor(Math.random() * 5);
         console.log(nmb);
-    }
+      }
     document.querySelector(".bck").style.backgroundColor = bckColor[nmb];
-} 
+    } 
+);
+
+
 
 
 
